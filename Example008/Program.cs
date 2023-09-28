8d95f86c6bf2a413ed060a51dd56759e28c14291
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-28 11:54:59 +0600

    код как на лекции, который не работает

---
+++ Example008/Program.cs
@@ -0,0 +1,27 @@
+﻿int Max(int arg1, int arg2, int arg3)
+{
+    result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+
+
+int a1 = 10;
+int b1 = 16;
+int c1 = 25;
+int a2 = 36;
+int b2 = 48;
+int c2 = 11;
+int a3 = 22;
+int b3 = 50;
+int c3 = 33;
+
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
