a9669a35b0d388cef168f6596a1f3909ea2e536b
Alyona Karnaukhova <alyona_2108@mail.ru>
2023-09-28 15:11:37 +0600

    нахождение индекса элемента массива

---
+++ Example010_ArrayMethod/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = {12, 56, 33, 7, 24, 33, 45, 8, 21};
+
+int n = array.Length;
+int find = 33;
+
+int index = 0;
+
+while(index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    //index = index + 1;
+    index++ ;
+}
